7cdcf134034f5f9b18d60d4083915758f1695850
rafael briones <e-rbnunez@neoris.com>
2019-12-03 13:56:04 -0600

    some progress

--- DS/datascience_module/descriptive_statistics/linear_model.py
+++ DS/datascience_module/descriptive_statistics/linear_model.py
@@ -1,21 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt  # Data visualizacion libraries
-import seaborn as sns
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
 
-class RegressionModel:
+class LinearModel:
 
-    def __init__(self, df_model, x_features, y_target):
+    __init__(self, df_model, x_features, y_target):
         self.df_model = df_model
         self.x_features = x_features
         self.y_target = y_target
 
-    def linear_model(self, df_source, test_percent=3.5):
-        sns.pairplot(df_source)
-
-    def execute_regression(self, test_size=.35, random_state=1):
+    def execute_regression(self, test_size=.35, random_state=1, show_scatter=False):
         m_X = self.df_model[self.x_features]
         m_y = self.df_model[self.y_target]
         X_train, X_test, y_train, y_test = train_test_split(
@@ -23,4 +19,10 @@ class RegressionModel:
         lm = LinearRegression()
         lm.fit(X_train, y_train)
         self.predictions = lm.predict(X_test)
-        plt.scatter(y_test, self.predictions)
+        if show_scatter:
+            plt.scatter(y_test, self.predictions)
+
+        self.df_actual_predict = pd.DataFrame({'Actual': y_test, 'Predicted': self.predictions})
+        self.df_coeff = pd.DataFrame(lm.coef_, X.columns, columns=['Coefficient'])
+
+
